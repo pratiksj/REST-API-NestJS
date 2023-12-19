32e72e23a646de86c3b7528ba5f53f691e1846dc
pratiksj <pratiksharai728@gmail.com>
2023-12-19 13:50:24 +0545

    feat:add prisma client to the article module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ArticlesModule],
 })
 export class AppModule {}
