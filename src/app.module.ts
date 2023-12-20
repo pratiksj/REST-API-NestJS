68894204f2356d39a95f80995e2e69477cd60918
pratiksj <pratiksharai728@gmail.com>
2023-12-20 16:45:25 +0545

    feat:set up to add authentication to the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ArticlesModule, UsersModule],
